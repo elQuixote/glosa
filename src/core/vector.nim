fb369d4c517edccb6be5995b0e9ba02224b5df8c
jrwells <rwells@modelab.is>
2018-04-09 15:20:26 -0700

    refactor(main): change template aliases to match addSelf

--- src/core/vector.nim
+++ src/core/vector.nim
@@ -8,14 +8,14 @@ type
   Vector4 = object
     x*, y*, z*, w*: float
 
-proc addSelf*(v1, v2: var Vector1): var Vector1 =
+proc addSelf*(v1: var Vector1, v2: Vector1): var Vector1 {.noinit.} =
   v1.x += v2.x
   result = v1
 
 proc addNew*(v1, v2: Vector1): Vector1 =
   result.x = v1.x + v2.x
 
-proc addSelf*(v1, v2: var Vector2): var Vector2 =
+proc addSelf*(v1: var Vector2, v2: Vector2): var Vector2 {.noinit.}  =
   v1.x += v2.x
   v1.y += v2.y
   result = v1
@@ -24,7 +24,7 @@ proc addNew*(v1, v2: Vector2): Vector2 =
   result.x = v1.x + v2.x
   result.y = v1.y + v2.y
 
-proc addSelf*(v1, v2: var Vector3): var Vector3 =
+proc addSelf*(v1: var Vector3, v2: Vector3): var Vector3 {.noinit.}  =
   v1.x += v2.x
   v1.y += v2.y
   v1.z += v2.z
@@ -35,7 +35,7 @@ proc addNew*(v1, v2: Vector3): Vector3 =
   result.y = v1.y + v2.y
   result.z = v1.z + v2.z
 
-proc addSelf*(v1, v2: var Vector4): var Vector4 =
+proc addSelf*(v1: var Vector4, v2: Vector4): var Vector4 {.noinit.}  =
   v1.x += v2.x
   v1.y += v2.y
   v1.z += v2.z
@@ -49,7 +49,7 @@ proc addNew*(v1, v2: Vector4): Vector4 =
   result.w = v1.w + v2.w
 
 
-template `+`*(v1, v2, result: var Vector1) = addSelf(v1, v2)
-template `+`*(v1, v2, result: var Vector2) = addSelf(v1, v2)
-template `+`*(v1, v2, result: var Vector3) = addSelf(v1, v2)
-template `+`*(v1, v2, result: var Vector4) = addSelf(v1, v2)
+template `+`*(v1: var Vector1, v2: Vector1): var Vector1 = addSelf(v1, v2)
+template `+`*(v1: var Vector2, v2: Vector2): var Vector2 = addSelf(v1, v2)
+template `+`*(v1: var Vector3, v2: Vector3): var Vector3 = addSelf(v1, v2)
+template `+`*(v1: var Vector4, v2: Vector4): var Vector4 = addSelf(v1, v2)
