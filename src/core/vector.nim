64e3489dbfbf52de4432c953b72c6f08a5e00329
jrwells <rwells@modelab.is>
2018-04-09 13:30:41 -0700

    feat(main): add object def for vectors

--- src/core/vector.nim
+++ src/core/vector.nim
@@ -1,1 +1,12 @@
 from concepts import Vector
+
+type
+  Vector1* = object
+    x*: float
+  Vector2* = object
+    x*, y*: float
+  Vector3 = object
+    x*, y*, z*: float
+  Vector4 = object
+    x*, y*, z*, w*: float
+
