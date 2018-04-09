42fd9fd3438c08eabd732e4ad7322a5a3d10cf3f
jrwells <rwells@modelab.is>
2018-04-09 15:35:58 -0700

    feat(main): add subtract to vectors, import concepts

--- src/core/vector.nim
+++ src/core/vector.nim
@@ -1,3 +1,5 @@
+import ./concepts
+
 type
   Vector1* = object
     x*: float
@@ -8,6 +10,7 @@ type
   Vector4 = object
     x*, y*, z*, w*: float
 
+# Addition
 proc addSelf*(v1: var Vector1, v2: Vector1): var Vector1 {.noinit.} =
   v1.x += v2.x
   result = v1
@@ -59,3 +62,56 @@ template `+=`*(v1: var Vector1, v2: Vector1): var Vector1 = addSelf(v1, v2)
 template `+=`*(v1: var Vector2, v2: Vector2): var Vector2 = addSelf(v1, v2)
 template `+=`*(v1: var Vector3, v2: Vector3): var Vector3 = addSelf(v1, v2)
 template `+=`*(v1: var Vector4, v2: Vector4): var Vector4 = addSelf(v1, v2)
+
+# Subtraction
+proc subtractSelf*(v1: var Vector1, v2: Vector1): var Vector1 {.noinit.} =
+  v1.x -= v2.x
+  result = v1
+
+proc subtractNew*(v1, v2: Vector1): Vector1 =
+  result.x = v1.x - v2.x
+
+proc subtractSelf*(v1: var Vector2, v2: Vector2): var Vector2 {.noinit.}  =
+  v1.x -= v2.x
+  v1.y -= v2.y
+  result = v1
+
+proc subtractNew*(v1, v2: Vector2): Vector2 =
+  result.x = v1.x - v2.x
+  result.y = v1.y - v2.y
+
+proc subtractSelf*(v1: var Vector3, v2: Vector3): var Vector3 {.noinit.}  =
+  v1.x -= v2.x
+  v1.y -= v2.y
+  v1.z -= v2.z
+  result = v1
+
+proc subtractNew*(v1, v2: Vector3): Vector3 =
+  result.x = v1.x - v2.x
+  result.y = v1.y - v2.y
+  result.z = v1.z - v2.z
+
+proc subtractSelf*(v1: var Vector4, v2: Vector4): var Vector4 {.noinit.}  =
+  v1.x -= v2.x
+  v1.y -= v2.y
+  v1.z -= v2.z
+  v1.w -= v2.w
+  result = v1
+
+proc subtractNew*(v1, v2: Vector4): Vector4 =
+  result.x = v1.x - v2.x
+  result.y = v1.y - v2.y
+  result.z = v1.z - v2.z
+  result.w = v1.w - v2.w
+
+# This is changed from design doc
+template `-`*(v1, v2: Vector1): Vector1 = subtractNew(v1, v2)
+template `-`*(v1, v2: Vector2): Vector2 = subtractNew(v1, v2)
+template `-`*(v1, v2: Vector3): Vector3 = subtractNew(v1, v2)
+template `-`*(v1, v2: Vector4): Vector4 = subtractNew(v1, v2)
+
+# This is added from design doc
+template `-=`*(v1: var Vector1, v2: Vector1): var Vector1 = subtractSelf(v1, v2)
+template `-=`*(v1: var Vector2, v2: Vector2): var Vector2 = subtractSelf(v1, v2)
+template `-=`*(v1: var Vector3, v2: Vector3): var Vector3 = subtractSelf(v1, v2)
+template `-=`*(v1: var Vector4, v2: Vector4): var Vector4 = subtractSelf(v1, v2)
