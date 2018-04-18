a4b09818930f9e11c872623b1200f506cf5271d7
jrwells <rwells@modelab.is>
2018-04-18 16:18:11 -0700

    test(main): add test for setting vectors

--- tests/core/test_vector.nim
+++ tests/core/test_vector.nim
@@ -128,3 +128,101 @@ suite "Copying a new Vector":
         v1.w != v2.w
         v1.w == ZERO_F
         v2.w == ONE_F
+
+suite "Setting a Vector to a single value":
+  test "Setting a Vector1":
+    block:
+      var 
+        v1 = Vector1(x: ZERO_F)
+      let
+        v2 = v1.setNew(ONE_F)
+      check:
+        v1.x != v2.x
+        v1.x == ZERO_F
+        v2.x == ONE_F
+      v1 = v1.setSelf(TWO_F)
+      check:
+        v1.x != v2.x
+        v1.x == TWO_F
+        v2.x == ONE_F
+  test "Setting a Vector2":
+    block:
+      var 
+        v1 = Vector2(x: ZERO_F, y: ZERO_F)
+      let
+        v2 = v1.setNew(ONE_F)
+      check:
+        v1.x != v2.x
+        v1.x == ZERO_F
+        v2.x == ONE_F
+        v1.y != v2.y
+        v1.y == ZERO_F
+        v2.y == ONE_F
+      v1 = v1.setSelf(TWO_F)
+      check:
+        v1.x != v2.x
+        v1.x == TWO_F
+        v2.x == ONE_F
+        v1.y != v2.y
+        v1.y == TWO_F
+        v2.y == ONE_F
+  test "Setting a Vector3":
+    block:
+      var 
+        v1 = Vector3(x: ZERO_F, y: ZERO_F, z: ZERO_F)
+      let
+        v2 = v1.setNew(ONE_F)
+      check:
+        v1.x != v2.x
+        v1.x == ZERO_F
+        v2.x == ONE_F
+        v1.y != v2.y
+        v1.y == ZERO_F
+        v2.y == ONE_F
+        v1.z != v2.z
+        v1.z == ZERO_F
+        v2.z == ONE_F
+      v1 = v1.setSelf(TWO_F)
+      check:
+        v1.x != v2.x
+        v1.x == TWO_F
+        v2.x == ONE_F
+        v1.y != v2.y
+        v1.y == TWO_F
+        v2.y == ONE_F
+        v1.z != v2.z
+        v1.z == TWO_F
+        v2.z == ONE_F
+  test "Setting a Vector4":
+    block:
+      var 
+        v1 = Vector4(x: ZERO_F, y: ZERO_F, z: ZERO_F, w: ZERO_F)
+      let
+        v2 = v1.setNew(ONE_F)
+      check:
+        v1.x != v2.x
+        v1.x == ZERO_F
+        v2.x == ONE_F
+        v1.y != v2.y
+        v1.y == ZERO_F
+        v2.y == ONE_F
+        v1.z != v2.z
+        v1.z == ZERO_F
+        v2.z == ONE_F
+        v1.w != v2.w
+        v1.w == ZERO_F
+        v2.w == ONE_F
+      v1 = v1.setSelf(TWO_F)
+      check:
+        v1.x != v2.x
+        v1.x == TWO_F
+        v2.x == ONE_F
+        v1.y != v2.y
+        v1.y == TWO_F
+        v2.y == ONE_F
+        v1.z != v2.z
+        v1.z == TWO_F
+        v2.z == ONE_F
+        v1.w != v2.w
+        v1.w == TWO_F
+        v2.w == ONE_F
