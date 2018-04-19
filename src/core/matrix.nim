8ae666b1e6502efe982a81b63628a0697f8b31d5
Luis Quinones <luis@complicitMatter.com>
2018-04-19 10:46:17 -0700

    feat(main): add clear templates to matrices

--- src/core/matrix.nim
+++ src/core/matrix.nim
@@ -187,3 +187,8 @@ proc copy*(m: Matrix44): Matrix44 =
         m10: m.m10, m11: m.m11, m12: m.m12, m13: m.m13,
         m20: m.m20, m21: m.m21, m22: m.m22, m23: m.m23, 
         m30: m.m30, m31: m.m31, m32: m.m32, m33: m.m33, matrix: m.matrix)
+
+#Clear
+template clear*(m: var Matrix32): var Matrix32 = set(m, 0.0)
+template clear*(m: var Matrix44): var Matrix44 = set(m, 0.0)
+
