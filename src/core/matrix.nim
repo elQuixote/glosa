e8db6ee2a102b03311e804f5d1a2dde7065c48ae
Luis Quinones <luis@complicitMatter.com>
2018-04-19 17:14:14 -0700

    refactor(main): remove properties, use single array for matrices. Replace all methods to use matrix arrays

--- src/core/matrix.nim
+++ src/core/matrix.nim
@@ -5,15 +5,9 @@ import hashes
 
 type 
     Matrix32* = object
-        m00*, m01*, m02*, 
-        m10*, m11*, m12*: float
         matrix*: array[2,array[3,float]] #NOTE: Do we want an array for matrix access?
     
     Matrix44* = object
-        m00*, m01*, m02*, m03*, 
-        m10*, m11*, m12*, m13*, 
-        m20*, m21*, m22*, m23*, 
-        m30*, m31*, m32*, m33*: float
         matrix*: array[4,array[4,float]] #NOTE: Do we want an array for matrix access?
 
 #Constructors
@@ -28,12 +22,6 @@ proc matrix32*(
     mx[1][0] = m10
     mx[1][1] = m11
     mx[1][2] = m12
-    result.m00 = m00
-    result.m01 = m01
-    result.m02 = m02
-    result.m10 = m10
-    result.m11 = m11
-    result.m12 = m12
     result.matrix = mx
 
 proc matrix44*(
@@ -59,33 +47,11 @@ proc matrix44*(
     mx[3][1] = m31
     mx[3][2] = m32
     mx[3][3] = m33
-    result.m00 = m00
-    result.m01 = m01
-    result.m02 = m02
-    result.m03 = m03
-    result.m10 = m10
-    result.m11 = m11
-    result.m12 = m12
-    result.m13 = m13
-    result.m20 = m20
-    result.m21 = m21
-    result.m22 = m22
-    result.m23 = m23
-    result.m30 = m30
-    result.m31 = m31
-    result.m32 = m32
-    result.m33 = m33
     result.matrix = mx
     
 #Set
 #NOTE : This is Added, not in design doc
 proc set*(m: var Matrix32, n: float): var Matrix32 {.noinit.} =
-    m.m00 = n
-    m.m01 = n
-    m.m02 = n
-    m.m10 = n
-    m.m11 = n
-    m.m12 = n
     m.matrix[0][0] = n
     m.matrix[0][1] = n
     m.matrix[0][2] = n
@@ -95,22 +61,6 @@ proc set*(m: var Matrix32, n: float): var Matrix32 {.noinit.} =
     result = m
 
 proc set*(m: var Matrix44, n: float): var Matrix44 {.noinit.} =
-    m.m00 = n
-    m.m01 = n
-    m.m02 = n
-    m.m03 = n
-    m.m10 = n
-    m.m11 = n
-    m.m12 = n
-    m.m13 = n
-    m.m20 = n
-    m.m21 = n
-    m.m22 = n
-    m.m23 = n
-    m.m30 = n
-    m.m31 = n
-    m.m32 = n
-    m.m33 = n
     m.matrix[0][0] = n
     m.matrix[0][1] = n
     m.matrix[0][2] = n
@@ -130,12 +80,6 @@ proc set*(m: var Matrix44, n: float): var Matrix44 {.noinit.} =
     result = m   
 
 proc set*(m: var Matrix32, a,b,c,d,e,f: float): var Matrix32 {.noinit.} =
-    m.m00 = a
-    m.m01 = b
-    m.m02 = c
-    m.m10 = d
-    m.m11 = e
-    m.m12 = f
     m.matrix[0][0] = a
     m.matrix[0][1] = b
     m.matrix[0][2] = c
@@ -145,22 +89,6 @@ proc set*(m: var Matrix32, a,b,c,d,e,f: float): var Matrix32 {.noinit.} =
     result = m
 
 proc set*(m: var Matrix44, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p: float): var Matrix44 {.noinit.} =
-    m.m00 = a
-    m.m01 = b
-    m.m02 = c
-    m.m03 = d
-    m.m10 = e   
-    m.m11 = f
-    m.m12 = g
-    m.m13 = h
-    m.m20 = i
-    m.m21 = j
-    m.m22 = k
-    m.m23 = l
-    m.m30 = m
-    m.m31 = n
-    m.m32 = o
-    m.m33 = p
     m.matrix[0][0] = a
     m.matrix[0][1] = b
     m.matrix[0][2] = c
@@ -181,16 +109,10 @@ proc set*(m: var Matrix44, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p: float): var Matrix44
 
 #Copy
 proc copy*(m: Matrix32): Matrix32 =
-    result = Matrix32(
-      m00: m.m00, m01: m.m01, m02: m.m02, 
-      m10: m.m10, m11: m.m11, m12: m.m12, matrix: m.matrix)
+    result = Matrix32(matrix: m.matrix)
   
 proc copy*(m: Matrix44): Matrix44 =
-    result = Matrix44(
-        m00: m.m00, m01: m.m01, m02: m.m02, m03: m.m03, 
-        m10: m.m10, m11: m.m11, m12: m.m12, m13: m.m13,
-        m20: m.m20, m21: m.m21, m22: m.m22, m23: m.m23, 
-        m30: m.m30, m31: m.m31, m32: m.m32, m33: m.m33, matrix: m.matrix)
+    result = Matrix44(matrix: m.matrix)
 
 #Clear
 template clear*(m: var Matrix32): var Matrix32 = set(m, 0.0)
@@ -198,38 +120,38 @@ template clear*(m: var Matrix44): var Matrix44 = set(m, 0.0)
 
 #Equals
 proc `==`*(m1, m2: Matrix32): bool = 
-   result = m1.m00 == m2.m00 and m1.m01 == m2.m01 and m1.m02 == m2.m02 and
-           m1.m10 == m2.m10 and m1.m11 == m2.m11 and m1.m12 == m2.m12   
+    result = m1.matrix[0][0] == m2.matrix[0][0] and m1.matrix[0][1] == m2.matrix[0][1] and m1.matrix[0][2] == m2.matrix[0][2] and m1
+        m1.matrix[1][0] == m2.matrix[1][0] and m1.matrix[1][1] == m2.matrix[1][1] and m1.matrix[1][2] == m2.matrix[1][2]  
 proc `==`*(m1, m2: Matrix44): bool =
-  result = m1.m00 == m2.m00 and m1.m01 == m2.m01 and m1.m02 == m2.m02 and m1.m03 == m2.m03 and
-           m1.m10 == m2.m10 and m1.m11 == m2.m11 and m1.m12 == m2.m12 and m1.m13 == m2.m13 and
-           m1.m20 == m2.m20 and m1.m21 == m2.m21 and m1.m22 == m2.m22 and m1.m23 == m2.m23 and
-           m1.m30 == m2.m30 and m1.m31 == m2.m31 and m1.m32 == m2.m32 and m1.m33 == m2.m33  
+    result = m1.matrix[0][0] == m2.matrix[0][0] and m1.matrix[0][1] == m2.matrix[0][1] and m1.matrix[0][2] == m2.matrix[0][2] and m1.matrix[0][3] == m2.matrix[0][3] and
+        m1.matrix[1][0] == m2.matrix[1][0] and m1.matrix[1][1] == m2.matrix[1][1] and m1.matrix[1][2] == m2.matrix[1][2] and m1.matrix[1][3] == m2.matrix[1][3] and
+        m1.matrix[2][0] == m2.matrix[2][0] and m1.matrix[2][1] == m2.matrix[2][1] and m1.matrix[2][2] == m2.matrix[2][2] and m1.matrix[2][3] == m2.matrix[2][2] and
+        m1.matrix[3][0] == m3.matrix[3][0] and m1.matrix[3][1] == m2.matrix[3][1] and m1.matrix[3][2] == m2.matrix[3][2] and m1.matrix[3][3] == m2.matrix[3][3]  
 
 # Hash
 proc hash*(m: Matrix32): hashes.Hash =
     result = !$(result !& 
-      hash(m.m00) !& hash(m.m01) !& hash(m.m02) !&
-      hash(m.m10) !& hash(m.m11) !& hash(m.m12))
+      hash(m.matrix[0][0]) !& hash(m.matrix[0][1]) !& hash(m.matrix[0][2]) !&
+      hash(m.matrix[1][0]) !& hash(m.matrix[1][1]) !& hash(m.matrix[1][2]))
   
 proc hash*(m: Matrix44): hashes.Hash =
     result = !$(result !& 
-        hash(m.m00) !& hash(m.m01) !& hash(m.m02) !& hash(m.m03) !&
-        hash(m.m10) !& hash(m.m11) !& hash(m.m12) !& hash(m.m13) !&
-        hash(m.m20) !& hash(m.m21) !& hash(m.m22) !& hash(m.m23) !&
-        hash(m.m30) !& hash(m.m31) !& hash(m.m32) !& hash(m.m33))
+        hash(m.matrix[0][0]) !& hash(m.matrix[0][1]) !& hash(m.matrix[0][2]) !& hash(m.matrix[0][3]) !&
+        hash(m.matrix[1][0]) !& hash(m.matrix[1][1]) !& hash(m.matrix[1][2]) !& hash(m.matrix[1][3]) !&
+        hash(m.matrix[2][0]) !& hash(m.matrix[2][1]) !& hash(m.matrix[2][2]) !& hash(m.matrix[2][3]) !&
+        hash(m.matrix[3][0]) !& hash(m.matrix[3][1]) !& hash(m.matrix[3][2]) !& hash(m.matrix[3][3]))
 
 # String
 # NOTE: Changed from design doc
 proc `$`*(m: Matrix32): string =
-    result = &"[{m.m00}, {m.m01}, {m.m02}, \n" &
-             &"{m.m10}, {m.m11}, {m.m12}]"
+    result = &"[{m.matrix[0][0]}, {m.matrix[0][1]}, {m.matrix[0][2]}, \n" &
+             &"{m.matrix[1][0]}, {m.matrix[1][1]}, {m.matrix[1][2]}]"
   
 proc `$`*(m: Matrix44): string =
-    result = &"[{m.m00}, {m.m01}, {m.m02}, {m.m03}, \n" &
-                &"{m.m10}, {m.m11}, {m.m12}, {m.m13}, \n" &
-                &"{m.m20}, {m.m21}, {m.m22}, {m.m23}, \n" &
-                &"{m.m30}, {m.m31}, {m.m32}, {m.m33}]"
+    result = &"[{m.matrix[0][0]}, {m.matrix[0][1]}, {m.matrix[0][2]}, {m.matrix[0][3]}, \n" &
+                &"{m.matrix[1][0]}, {m.matrix[1][1]}, {m.matrix[1][2]}, {m.matrix[1][3]}, \n" &
+                &"{m.matrix[2][0]}, {m.matrix[2][1]}, {m.matrix[2][2]}, {m.matrix[2][3]}, \n" &
+                &"{m.matrix[3][0]}, {m.matrix[3][1]}, {m.matrix[3][2]}, {m.matrix[3][3]}]"
 
 #Transpose
 proc transposeSelf*(m: var Matrix32): var Matrix32 {.noinit.} =
