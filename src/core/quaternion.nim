19782aa9e57271288f6f8a09f5353d72e46ef19b
Luis Quinones <luis@complicitMatter.com>
2018-04-23 11:12:39 -0700

    feat(main): add constructor to quaternion

--- src/core/quaternion.nim
+++ src/core/quaternion.nim
@@ -7,4 +7,10 @@ import hashes
 type 
     Quaternion* = object
         x*,y*,z*,w* : float 
-        
+
+#Constructor
+proc quaternion(x, y, z, w: float): Quaternion =
+    result.x = x
+    result.y = y
+    result.z = z
+    result.w = w
