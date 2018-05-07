a6a956dae027de5acc9b016802723b0937f869c6
Luis Quinones <luis@complicitMatter.com>
2018-05-07 10:39:55 -0700

    feat(main): add magnitude proc wrappers to vector2 and vector3

--- wrappers/core/vectors.nim
+++ wrappers/core/vectors.nim
@@ -26,6 +26,7 @@ proc normalizeSelf_v2*(v: var Vector2, m: cdouble = 1.0): Vector2 {.cdecl, expor
 proc angleBetween_v2*(v1, v2: Vector2): cdouble {.cdecl, exportc, dynlib.} = angleBetween(v1,v2)
 proc dimension_v2*(v: Vector2): int {.cdecl, exportc, dynlib.} = dimension(v)
 proc stringify_v2*(v: Vector2): string {.cdecl, exportc, dynlib.} = $(v)
+proc magnitude_v2*(v: Vector2): cdouble {.cdecl, exportc, dynlib.} = length(v)
 
 # Vector3 Proc Wraps
 proc copy_v3*(v1: Vector3): Vector3 {.cdecl, exportc, dynlib.} = copy(v1)
@@ -55,3 +56,4 @@ proc normalizeSelf_v3*(v: var Vector3, m: cdouble = 1.0): Vector3 {.cdecl, expor
 proc angleBetween_v3*(v1, v2: Vector3): cdouble {.cdecl, exportc, dynlib.} = angleBetween(v1,v2)
 proc dimension_v3*(v: Vector3): int {.cdecl, exportc, dynlib.} = dimension(v)
 proc stringify_v3*(v: Vector3): string {.cdecl, exportc, dynlib.} = $(v)
+proc magnitude_v3*(v: Vector3): cdouble {.cdecl, exportc, dynlib.} = length(v)
