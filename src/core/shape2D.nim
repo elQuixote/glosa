276bd8b42d4aed2e5ae46850fcab9653fa73e4c8
Luis Quinones <luis@complicitMatter.com>
2018-05-09 13:15:03 -0700

    feat(main): add shape2d types

--- src/core/shape2D.nim
+++ src/core/shape2D.nim
@@ -31,3 +31,17 @@ Vertices
 from math import arctan2, arccos, sqrt
 from strformat import `&`
 import hashes
+
+from ./Vector import Vector2,Vector3
+
+type
+  Circle* = object
+    center*: Vector2
+    radius*: float
+  Polygon* = object
+    vertices*: seq[Vector2]
+  Sphere* = object
+    center*: Vector2
+    radius*: float
+    
+    
