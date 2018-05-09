34540c5307542e3e4ad87d92db5442e242e513ae
Luis Quinones <luis@complicitMatter.com>
2018-05-09 13:06:08 -0700

    feat(main): add concepts and imports to shape2d

---
+++ src/core/shape2D.nim
@@ -0,0 +1,33 @@
+from ./concepts import
+Compare,
+Equals,
+Hash,
+Transform,
+Dimension,
+Set,
+Clear,
+Copy,
+String,
+Centroid,
+Shape2,
+Closest,
+Vertices
+
+export
+Compare,
+Equals,
+Hash,
+Transform,
+Dimension,
+Set,
+Clear,
+Copy,
+String,
+Centroid,
+Shape2,
+Closest,
+Vertices
+
+from math import arctan2, arccos, sqrt
+from strformat import `&`
+import hashes
