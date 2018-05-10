931f1f706846cd39f45c64e231fda62d25fc6667
Luis Quinones <luis@complicitMatter.com>
2018-05-10 13:18:38 -0700

    update(main): fix cross product for vector2 calculations, update shape proc signatures for generics

--- src/core/shape.nim
+++ src/core/shape.nim
@@ -1,86 +1,87 @@
 from ./concepts import
-Vector,
-Equals,
-Hash,
-Transform,
-Dimension,
-Set,
-Clear,
-Copy,
-String,
-Centroid,
-Shape2,
-Closest,
-Vertices
+  Equals,
+  Hash,
+  Transform,
+  Dimension,
+  Set,
+  Clear,
+  Copy,
+  String,
+  Centroid,
+  Shape2,
+  Closest,
+  Vertices
 
 export
-Vector,
-Equals,
-Hash,
-Transform,
-Dimension,
-Set,
-Clear,
-Copy,
-String,
-Centroid,
-Shape2,
-Closest,
-Vertices
+  Equals,
+  Hash,
+  Transform,
+  Dimension,
+  Set,
+  Clear,
+  Copy,
+  String,
+  Centroid,
+  Shape2,
+  Closest,
+  Vertices
 
 from math import arctan2, arccos, sqrt
 from strformat import `&`
 import hashes
 
-import ../../src/core/vector
+import ./vector
 
 type
-  Circle* = object
+  
+  Circle*[Vector] = object
     center*: Vector
     radius*: float
-  Polygon* = object
+  
+  Polygon*[Vector] = object
     vertices*: seq[Vector]
-  Sphere* = object
+  
+  Sphere*[Vector] = object
     center*: Vector
     radius*: float
-
+  
 # Polygon
 # Constuctors
-proc polygon*(points: seq[Vector]): Polygon =
+proc polygon*[Vector](points: seq[Vector]): Polygon[Vector] =
   result.vertices = @[]
   result.vertices = points
 
-proc polygon*(): Polygon =
+proc polygon*[Vector](): Polygon[Vector] =
   result.vertices = @[]
 
 # ***************************************
 #     Polygon implementation
 # ***************************************
 # NOTE: This is added from design doc
-proc addVertex*(p: var Polygon, x, y: float): Polygon = 
+proc addVertex*[Vector](p: var Polygon[Vector], x, y: float): Polygon[Vector] = 
   if not p.contains(vector2(x,y)):
     p.vertices.add(vector2(x, y))
   else: raise newException(AccessViolationError, "Attempting to add a vertex which alreaday exists")
   result = p
 
-proc addVertex*(p: var Polygon, x, y, z: float): Polygon = 
+proc addVertex*[Vector](p: var Polygon[Vector], x, y, z: float): Polygon[Vector] = 
   if not p.contains(vector3(x,y,z)):
     p.vertices.add(vector3(x, y, z))
   else: raise newException(AccessViolationError, "Attempting to add a vertex which alreaday exists")
   result = p
 
-proc addVertex*(p: var Polygon, v: Vector): Polygon =
+proc addVertex*[Vector](p: var Polygon[Vector], v: Vector): Polygon[Vector] =
   if not p.contains(v):
     p.vertices.add(v)
   else: raise newException(AccessViolationError, "Attempting to add a vertex which alreaday exists")
   result = p
 
 # NOTE: This is added from design doc
-proc pointCount(p: Polygon): int = 
+proc pointCount*[Vector](p: Polygon[Vector]): int = 
   result = len(p.vertices)
 
 # NOTE: This is added from design doc
-proc contains*(p: var Polygon, v: Vector): bool =
+proc contains*[Vector](p: var Polygon[Vector], v: Vector): bool =
   var hit : bool
   for vert in p.vertices:
     if vert == v:
@@ -90,56 +91,81 @@ proc contains*(p: var Polygon, v: Vector): bool =
   result = hit
 
 # Equals (compares points in polygon)
-proc `==`*(p1,p2: Polygon): bool =
+proc `==`*[Vector](p1,p2: Polygon[Vector]): bool =
   if p1.pointCount() != p2.pointCount(): return false
   var hit : bool
-  for i in 0..p1.pointCount()-1:
+  for i in 0..<p1.pointCount():
     if p1.vertices[i] != p2.vertices[i]: return false
     else: hit = true
   result = hit
 
 # Non Equals
-proc `!=`*(p1,p2: Polygon): bool = 
-result = not (p1 == p2)
+proc `!=`*[Vector](p1,p2: Polygon[Vector]): bool = 
+  result = not (p1 == p2)
 
 # Hash
-proc hash*(p: Polygon): hashes.Hash =
+proc hash*[Vector](p: Polygon[Vector]): hashes.Hash =
   for vert in p.vertices:
     result = !$(result !& hash(vert))
 
 # Clear
-proc clear*(p: var Polygon): var Polygon =
+proc clear*[Vector](p: var Polygon[Vector]): var Polygon[Vector] =
   p.vertices = @[]
   result = p
 
 # Dimension
-proc dimension*(p: Polygon): int =
+proc dimension*[Vector](p: Polygon[Vector]): int =
   if p.pointCount() == 0: raise newException(AccessViolationError, "Polygon has no vertices")
   result = p.vertices.dimension()
 
 # Copy
-proc copy*(p: Polygon): Polygon = 
+proc copy*[Vector](p: Polygon[Vector]): Polygon[Vector] = 
   result = Polygon(vertices: p.vertices)
 
 # String
-proc `$`*(p: Polygon): string =
+proc `$`*[Vector](p: Polygon[Vector]): string =
   result = ""
   for vert in p.vertices:
     result.add($vert & ",")
 
 # Shape2 Concept
 # Area
-proc area*(p: Polygon): float =
-  for i in 0..p.pointCount()-1:
+proc area*[Vector](p: Polygon[Vector]): float =
+  for i in 0..<p.pointCount():
     var a = p.vertices[i]
     var b = p.vertices[(i + 1) mod p.pointCount()]
     result += (a.x * b.y)
     result -= (a.y * b.x)
   result *= 0.5
 
-proc perimeter*(p: Polygon): float =
-  for i in 0..p.pointCount()-1:
+# Perimeter (the circumference)
+proc perimeter*[Vector](p: Polygon[Vector]): float =
+  for i in 0..<p.pointCount():
     result += p.vertices[i].distanceTo(p.vertices[(i + 1) mod p.pointCount()])
+
+# Centroid
+proc centroid*[Vector](p: Polygon[Vector]): Vector =
+  var vecRef : Vector = p.vertices[0]
+  var vecRef2 = vecRef.copy()
+  var vec = vecRef2.clear()
+  for i in 0..<p.pointCount():
+    var a = p.vertices[i]
+    var b = p.vertices[(i + 1) mod p.pointCount()]
+    var cp = cross(a, b)
+    vec += (a + b) * cp
+  result = vec.multiplySelf(1.0 / (6 * p.area()))
+
+proc average*[Vector](p: Polygon[Vector]): Vector =
+  var vecZ : Vector = p.vertices[0]
+  var vecY = vecZ.copy()
+  var vec = vecY.clear()
+  for i in 0..<p.pointCount():
+    vec += p.vertices[i]
+  vec /= (float)p.pointCount()
+  result = vec
+
+# Add Planar check 
+
   
 
   
