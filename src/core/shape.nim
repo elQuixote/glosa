5c93c6ae15ec8935df7b3e931168059eaaddf060
Luis Quinones <luis@complicitMatter.com>
2018-05-10 09:55:04 -0700

    style(main): adjust indentation in import export for shape.nim

--- src/core/shape.nim
+++ src/core/shape.nim
@@ -1,32 +1,32 @@
 from ./concepts import
-Vector,
-Equals,
-Hash,
-Transform,
-Dimension,
-Set,
-Clear,
-Copy,
-String,
-Centroid,
-Shape2,
-Closest,
-Vertices
+  Vector,
+  Equals,
+  Hash,
+  Transform,
+  Dimension,
+  Set,
+  Clear,
+  Copy,
+  String,
+  Centroid,
+  Shape2,
+  Closest,
+  Vertices
 
 export
-Vector,
-Equals,
-Hash,
-Transform,
-Dimension,
-Set,
-Clear,
-Copy,
-String,
-Centroid,
-Shape2,
-Closest,
-Vertices
+  Vector,
+  Equals,
+  Hash,
+  Transform,
+  Dimension,
+  Set,
+  Clear,
+  Copy,
+  String,
+  Centroid,
+  Shape2,
+  Closest,
+  Vertices
 
 from math import arctan2, arccos, sqrt
 from strformat import `&`
