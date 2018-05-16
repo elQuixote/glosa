2dc615ad49230f2ef3eeea6dacdc98030ef05cba
Luis Quinones <luis@complicitMatter.com>
2018-05-15 17:00:11 -0700

    feat(main): add magnitudeSquared to vector2 and vector3 wrappers

--- src/wrappers/core/wrapper_vector.nim
+++ src/wrappers/core/wrapper_vector.nim
@@ -45,6 +45,7 @@ proc toVector3*(v1: Vector2, z: cdouble): Vector3 {.cdecl, exportc, dynlib.} = v
 proc fromArray_v2*(a: array[2, cdouble]): Vector2 {.cdecl, exportc, dynlib.} = vector2(a)
 proc fromPolar_v2*(r, theta: cdouble): Vector2 {.cdecl, exportc, dynlib.} = fromPolar(r, theta)
 proc randomize_v2*(v: var Vector2, maxX, maxY: cdouble): Vector2 {.cdecl, exportc, noinit, dynlib.} = randomize(v, maxX, maxY)
+proc magnitudeSquared_v2*(v: Vector2): cdouble {.cdecl, exportc, dynlib.} = magnitudeSquared(v)
 
 # Vector3 Proc Wraps
 proc copy_v3*(v1: Vector3): Vector3 {.cdecl, exportc, dynlib.} = copy(v1)
@@ -94,3 +95,4 @@ proc toVector2*(v: Vector3): Vector2 {.cdecl, exportc, dynlib.} = vector2(v)
 proc fromArray_v3*(a: array[3, cdouble]): Vector3 {.cdecl, exportc, dynlib.} = vector3(a)
 proc fromSpherical_v3*(r, theta, phi: cdouble): Vector3 {.cdecl, exportc, dynlib.} = fromSpherical(r, theta, phi)
 proc randomize_v3*(v: var Vector3, maxX, maxY, maxZ: cdouble): Vector3 {.cdecl, exportc, noinit, dynlib.} = randomize(v, maxX, maxY, maxZ)
+proc magnitudeSquared_v3*(v: Vector3): cdouble {.cdecl, exportc, dynlib.} = magnitudeSquared(v)
