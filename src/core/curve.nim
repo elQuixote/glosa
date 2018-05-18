a66316a1fa283a0c3b036c3478cf5e45f09db475
jrwells <rwells@modelab.is>
2018-05-18 10:10:42 -0700

    feat(main): add equals, hash, dimension, copy, and string predication

--- src/core/curve.nim
+++ src/core/curve.nim
@@ -1,3 +1,14 @@
+from ./concepts import
+  Equals,
+  Hash,
+  Transform,
+  Dimension,
+  # Clear,
+  Copy,
+  String,
+  # Centroid,
+  Closest
+
 from ./types import
   NurbsCurve
 
@@ -5,6 +16,24 @@ from ./errors import
   InvalidDegreeError,
   InvalidKnotsError,
   InvalidInterpolationError
+
+export
+  Equals,
+  Hash,
+  Transform,
+  Dimension,
+  # Clear,
+  Copy,
+  String,
+  # Centroid,
+  Closest,
+  NurbsCurve,
+  InvalidDegreeError,
+  InvalidKnotsError,
+  InvalidInterpolationError
+
+from strformat import `&`
+import hashes
 
 proc isValidNurbsCurveData[Vector](degree: int, controlPoints: seq[Vector], knots: seq[float]): bool =
   result = true
@@ -28,17 +57,56 @@ proc isValidInterpolationPoints[Vector](degree: int, points: seq[Vector]): bool 
 
 # Constructors
 # From data
-proc nurbsCurve*[Vector](degree: int, controlPoints: seq[Vector], knots: seq[float]): NurbsCurve =
+proc nurbsCurve*[Vector](degree: int, controlPoints: seq[Vector], knots: seq[float]): NurbsCurve[Vector] =
   if isValidNurbsCurveData(degree, controlPoints, knots):
     result = NurbsCurve(degree: degree, controlPoints: controlPoints, knots: knots)
 
 # From interpolation through points
 # TODO
-proc nurbsCurve*[Vector](degree: int, points: seq[Vector]): NurbsCurve =
+proc nurbsCurve*[Vector](degree: int, points: seq[Vector]): NurbsCurve[Vector] =
   if isValidInterpolationPoints(degree, points):
     discard
 
-proc copy*[Vector](nc: NurbsCurve): NurbsCurve =
+proc `==`*[Vector](nc1: NurbsCurve[Vector], nc2: NurbsCurve[Vector]): bool =
+  result = true
+  if (nc1.degree != nc2.degree) or
+    (len(nc1.controlPoints) != len(nc2.controlPoints)) or
+      (len(nc1.knots) != len(nc2.knots)):
+    return false
+  for i, v in nc1.controlPoints:
+    if (v != nc2.controlPoints[i]):
+      return false
+  for i, v in nc1.knots:
+    if (v != nc2.knots[i]):
+      return false
+
+proc `!=`*[Vector](nc1: NurbsCurve[Vector], nc2: NurbsCurve[Vector]): bool =
+  result = not (nc1 == nc2)
+
+proc hash*[Vector](nc: NurbsCurve[Vector]): hashes.Hash =
+  result = !$(result !& hash(nc.degree))
+  for i, v in nc.controlPoints:
+    result = result !& hash(v)
+  for i, v in nc.knots:
+    result = result !& hash(v)
+
+proc dimension*[Vector](nc: NurbsCurve[Vector]): int =
+  if len(nc.controlPoints) != 0:
+    result = dimension(nc.vertices[0])
+
+proc copy*[Vector](nc: NurbsCurve[Vector]): NurbsCurve[Vector] =
   result = nurbsCurve(nc.degree, nc.controlPoints, nc.knots)
 
-
+proc `$`*[Vector](nc: NurbsCurve[Vector]): string =
+  result = "{ degree: " & nc.degree
+  if len(nc.controlPoints) > 0:
+    result &= ", controlPoints: [" & $nc.controlPoints[0]
+    for v in nc.controlPoints[1..^1]:
+      result &= ", " & $v
+    result &= "]"
+  if len(nc.knots) > 0:
+    result &= ", knots: [" & $nc.knots[0]
+    for v in nc.knots[1..^1]:
+      result &= ", " & $v
+    result &= "]"
+  result &= " }"
