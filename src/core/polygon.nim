ad8ecd0cdba6a88c187d8cc2e37405ae60346e63
Luis Quinones <luis@complicitMatter.com>
2018-05-31 16:33:24 -0700

    refactor(main): refactor reverse in polygon to also reverse line segments as well

--- src/core/polygon.nim
+++ src/core/polygon.nim
@@ -141,13 +141,19 @@ iterator faces*[Vector](p: Polygon[Vector]): LineSegment[Vector] =
     yield s
 
 # NOTE: This is added from design doc
-proc reverse*[Vector](p: var Polygon[Vector]): var Polygon[Vector] =
+proc reverse*[Vector](p: Polygon[Vector]): Polygon[Vector] =
   #result = polygon(reverse(p.polyline))
   var list = newSeq[Vector](len(p.polyline.vertices))
   for i, x in p.polyline.vertices:
     list[p.polyline.vertices.high-i] = x
-  p.polyline.vertices = list 
-  result = p
+  #p.polyline.vertices = list 
+  #result = p
+  if areClosed(p.polyline.segments):
+    var p = polyline(list, true)
+    result = Polygon[Vector](polyline: p)
+  else:
+    var p = polyline(list, false)
+    result = Polygon[Vector](polyline: p)
 
 # NOTE: This is added from design doc
 proc contains*[Vector](p: Polygon[Vector], v: Vector): bool =
