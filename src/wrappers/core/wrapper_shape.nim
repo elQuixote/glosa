7fa846409ad5723b3b01caf6b440e876da25445b
Luis Quinones <luis@complicitMatter.com>
2018-06-01 14:43:16 -0700

    feat(main): add contains to shape wrapper

--- src/wrappers/core/wrapper_shape.nim
+++ src/wrappers/core/wrapper_shape.nim
@@ -21,4 +21,18 @@ proc circle_v4*(s: cstring): cstring {.cdecl, exportc, dynlib.} =
   result = toJson(circle4FromJson($s))
   tearDownForeignThreadGc()
 
+# Contains
+proc contains_v2_polygon*(s: cstring, v: Vector2): bool {.cdecl, exportc, dynlib.} = 
+  setupForeignThreadGc()
+  result = containsPoint(circle2FromJson($s), v)
+  tearDownForeignThreadGc()
 
+proc contains_v3_polygon*(s: cstring, v: Vector3): bool {.cdecl, exportc, dynlib.} = 
+  setupForeignThreadGc()
+  result = containsPoint(circle3FromJson($s), v)
+  tearDownForeignThreadGc()
+
+proc contains_v4_polygon*(s: cstring, v: Vector4): bool {.cdecl, exportc, dynlib.} = 
+  setupForeignThreadGc()
+  result = containsPoint(circle4FromJson($s), v)
+  tearDownForeignThreadGc()
