93c138648d04e5b36cfc809a5dd7e28b30f1c42e
Luis Quinones <luis@complicitMatter.com>
2018-06-01 14:45:13 -0700

    feat(main): add area wrapper procs to shape wrapper

--- src/wrappers/core/wrapper_shape.nim
+++ src/wrappers/core/wrapper_shape.nim
@@ -1,4 +1,5 @@
 import ../../core/vector
+import ../../core/shape
 import json
 from ../../core/matrix import Matrix44, Matrix33
 
@@ -22,17 +23,33 @@ proc circle_v4*(s: cstring): cstring {.cdecl, exportc, dynlib.} =
   tearDownForeignThreadGc()
 
 # Contains
-proc contains_v2_polygon*(s: cstring, v: Vector2): bool {.cdecl, exportc, dynlib.} = 
+proc contains_v2_circle*(s: cstring, v: Vector2): bool {.cdecl, exportc, dynlib.} = 
   setupForeignThreadGc()
   result = containsPoint(circle2FromJson($s), v)
   tearDownForeignThreadGc()
 
-proc contains_v3_polygon*(s: cstring, v: Vector3): bool {.cdecl, exportc, dynlib.} = 
+proc contains_v3_circle*(s: cstring, v: Vector3): bool {.cdecl, exportc, dynlib.} = 
   setupForeignThreadGc()
   result = containsPoint(circle3FromJson($s), v)
   tearDownForeignThreadGc()
 
-proc contains_v4_polygon*(s: cstring, v: Vector4): bool {.cdecl, exportc, dynlib.} = 
+proc contains_v4_circle*(s: cstring, v: Vector4): bool {.cdecl, exportc, dynlib.} = 
   setupForeignThreadGc()
   result = containsPoint(circle4FromJson($s), v)
+  tearDownForeignThreadGc()
+
+# Area
+proc area_v2_circle*(s: cstring): cdouble {.cdecl, exportc, dynlib.} = 
+  setupForeignThreadGc()
+  result = area(circle2FromJson($s))
+  tearDownForeignThreadGc()
+
+proc area_v3_circle*(s: cstring): cdouble {.cdecl, exportc, dynlib.} = 
+  setupForeignThreadGc()
+  result = area(circle3FromJson($s))
+  tearDownForeignThreadGc()
+
+proc area_v4_circle*(s: cstring): cdouble {.cdecl, exportc, dynlib.} = 
+  setupForeignThreadGc()
+  result = area(circle4FromJson($s))
   tearDownForeignThreadGc()
